a2f729fa4d4d3cf723c88ecb7a3e0d2240f0dd9f
prem22k <premsai224k@gmail.com>
2026-06-17 17:37:01 +0530

    chore: update generated dev service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "/index.html",
-    "revision": "0.9l5l2nrloso"
+    "revision": "0.1ctq31atsjg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
