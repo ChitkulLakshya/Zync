393f052e4e0e97e29d2a0043062381791678cda0
prem22k <premsai224k@gmail.com>
2026-06-18 12:09:35 +0530

    chore: update dev service worker cache revision

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "/index.html",
-    "revision": "0.1ctq31atsjg"
+    "revision": "0.445hkh7iv64"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
