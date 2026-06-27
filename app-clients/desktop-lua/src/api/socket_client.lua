c0b4e676d5f3895ed4b645bf7ab59f49bec62a75
Chitkul Lakshya <chitkullakshya@gmail.com>
2026-06-27 01:00:00 +0000

    feat(desktop): add socket event subscription system

--- app-clients/desktop-lua/src/api/socket_client.lua
+++ app-clients/desktop-lua/src/api/socket_client.lua
@@ -3,3 +3,7 @@ function SocketClient.connect(url)
     print("Connecting to Zync WebSocket at " .. url)
 end
 return SocketClient
+
+function SocketClient.on(event, callback)
+    print("Listening to socket event: " .. event)
+end
