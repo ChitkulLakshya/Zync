286818ec99a47073948adbcafb1f4a9b1416d80b
Chitkul Lakshya <chitkullakshya@gmail.com>
2026-06-27 07:00:00 +0000

    feat(desktop): integrate OfflineDB into Kanban state store

--- app-clients/desktop-lua/src/state/kanban_state.lua
+++ app-clients/desktop-lua/src/state/kanban_state.lua
@@ -4,3 +4,8 @@ function state.loadBoard(token, teamId)
     state.activeBoard = projects.getProjects(token, teamId)[1]
 end
 return state
+
+local offline_db = require("utils.offline_db")
+function state.cacheBoard()
+    offline_db.save("active_board", state.activeBoard)
+end
